33744bd1e2e5664c25ae04cf43834170273922d5
junex0x <huynhhaiyen.hie@gmail.com>
2026-07-20 17:08:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // config.js
 const CONFIG = {
-    PASSWORD: "june3117",           // ← Thay mật khẩu ở đây, rất dễ
+    PASSWORD: "june3117",           // Mật khẩu ban đầu (sẽ tự đổi sau)
     TELEGRAM_BOT_TOKEN: "7825998395:AAHdXL_wL4BMhj-yYVefnMAtHqwz8rxjEKw",
     TELEGRAM_CHAT_ID: "6217916397",
-    SITE_NAME: "JUNE - Shadowrocket"
+    SITE_NAME: "JUNE - Shadowrocket",
+    MAX_ATTEMPTS: 3
 };
